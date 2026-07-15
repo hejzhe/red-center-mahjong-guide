486237d54cfeb4ae205406db740448145fe24ff8
土司儿 <stonetu@iMac.local>
2026-07-15 13:20:09 +0800

    Add kong reversal lessons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mahjong-guide-v5';
+const CACHE_NAME = 'mahjong-guide-v6';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
