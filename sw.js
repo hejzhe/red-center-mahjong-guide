8d967a85cac656ad537ac49545214c06cf6a9e89
土司儿 <stonetu@iMac.local>
2026-07-15 19:29:49 +0800

    Add five-pair boundary lessons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mahjong-guide-v9';
+const CACHE_NAME = 'mahjong-guide-v10';
 const ASSETS = ['./', './index.html', './quick-reference.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
